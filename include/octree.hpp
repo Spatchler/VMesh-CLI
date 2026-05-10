c517c245aecd1079bf6e478b8e32499aeab428a7
spatchler <icodecubes@outlook.com>
2026-05-10 20:17:30 +0100

    Completely rewrote octree implementation

--- include/octree.hpp
+++ include/octree.hpp
@@ -1,52 +1,40 @@
 #pragma once
 
+#include <print>
 #include <vector>
 #include <array>
-#include <algorithm>
 #include <limits>
-#include <string>
+#include <memory>
 #include "VMesh/voxelGrid.hpp"
 
-class Node {
-public:
-  Node(bool pIsLeaf, bool pIsAir, uint pSize, glm::uvec3 pOrigin);
-
-  void createChildren(std::vector<std::array<uint32_t, 8>>& pIndices, std::vector<Node*>& pQueue);
-  void evaluateChildrenIndices(std::vector<std::array<uint32_t, 8>>& pIndices);
-  void generate(VMesh::VoxelGrid& pGrid, std::vector<Node*>& pQueue, uint64_t& pCompletedCount);
-  void generateBottomUp(VMesh::VoxelGrid& pGrid, std::vector<Node*>& pQueue, uint64_t& pCompletedCount);
-
-  static uint toChildIndex(glm::uvec3 pPos);
-  static glm::uvec3 toPos(uint8_t pChildIndex);
-
-  static void destroy();
-  
-  bool isLeaf = false, isAir = false;
+struct Node {
+  Node() = default;
+  Node(uint32_t i) : index(i) {};
   uint32_t index = 0;
-
-  glm::uvec3 origin = glm::uvec3(0);
-  uint size = 0;
-
-  static Node* sSolid;
-  static Node* sAir;
-  std::array<Node*, 8> children = {{sAir}};
+  std::array<std::shared_ptr<Node>, 8> children;
 };
 
-class SparseVoxelOctree {
+class Octree {
 public:
-  SparseVoxelOctree(uint pSize);
-  SparseVoxelOctree(VMesh::VoxelGrid& pGrid, uint64_t* pCompletedCount = NULL);
+  Octree(uint pResolution, uint pPaletteSize);
+  Octree(VMesh::VoxelGrid& pGrid, uint64_t* pCompletedCount = NULL);
 
-  void attachSVO(SparseVoxelOctree& pSVO, const glm::uvec3& pOrigin);
+  void attach(Octree& pOctree, glm::uvec3& pOrigin);
 
   std::vector<std::array<uint32_t, 8>> generateIndices();
 
-  void write(const std::string& pPath);
+  void resizePalette(uint pSize);
 
-  void destroy();
+  uint getResolution();
 
-  uint32_t mSize;
-  uint mMaxDepth;
+  void write(std::string pPath);
 
-  Node* mRootNode = NULL;
+  void generateNode();
+  void processNode(std::shared_ptr<Node> pNode, std::vector<std::array<uint32_t*, 8>>& pArangement, std::vector<std::shared_ptr<Node>>& pQueue);
+  static uint toChildIndex(const glm::uvec3& pPos);
+  static glm::uvec3 toChildPos(uint8_t pIndex);
+
+  uint mResolution;
+  std::vector<std::shared_ptr<Node>> mNodes;
+  std::vector<std::shared_ptr<Node>> mPalette;
 };
--- src/octree.cpp
+++ src/octree.cpp
@@ -1,87 +1,153 @@
-#include "octree.hpp"
+#include <octree.hpp>
 
-Node* Node::sSolid = new Node(true, false, 0, glm::uvec3(0));
-Node* Node::sAir = new Node(true, true, 0, glm::uvec3(0));
-
-Node::Node(bool pIsLeaf, bool pIsAir, uint pSize, glm::uvec3 pOrigin)
-:isLeaf(pIsLeaf), isAir(pIsAir), size(pSize), origin(pOrigin) {
-  children = {sAir, sAir, sAir, sAir, sAir, sAir, sAir, sAir};
+Octree::Octree(uint pResolution, uint pPaletteSize)
+:mResolution(pResolution) {
+  // Init palette
+  for (uint i = 0; i <= pPaletteSize; ++i)
+    mPalette.emplace_back(std::make_shared<Node>(std::numeric_limits<uint32_t>::max() - pPaletteSize + i));
 }
 
-void Node::createChildren(std::vector<std::array<uint32_t, 8>>& pIndices, std::vector<Node*>& pQueue) {
-  for (Node* i: children) if (!i->isLeaf) {
-    i->index = pIndices.size();
-    pQueue.push_back(i);
-    pIndices.emplace_back();
-  }
-}
+Octree::Octree(VMesh::VoxelGrid& pGrid, uint64_t* pCompletedCount)
+:mResolution(pGrid.getResolution()) {
+  uint64_t completedCount;
+  if (!pCompletedCount)
+    pCompletedCount = &completedCount;
 
-void Node::evaluateChildrenIndices(std::vector<std::array<uint32_t, 8>>& pIndices) {
-  for (uint8_t i = 0; i < 8; ++i) {
-    if (!children[i]->isLeaf)    pIndices[index][i] = children[i]->index;
-    else if (children[i]->isAir) pIndices[index][i] = std::numeric_limits<uint32_t>::max() - 1;
-    else                         pIndices[index][i] = std::numeric_limits<uint32_t>::max();
-  }
-}
+  // Init palette
+  for (uint i = 0; i <= pGrid.mPalette.size(); ++i)
+    mPalette.emplace_back(std::make_shared<Node>(std::numeric_limits<uint32_t>::max() - pGrid.mPalette.size() + i));
 
-void Node::generate(VMesh::VoxelGrid& pGrid, std::vector<Node*>& pQueue, uint64_t& pCompletedCount) {
-  uint childSize = size >> 1;
-  uint64_t childVolume = childSize;
-  childVolume = childVolume * childVolume * childVolume;
-
-  if (childSize == 0) return;
-  if (childSize == 1) {
-    for (uint i = 0; i < 8; ++i,++pCompletedCount) {
-      glm::uvec3 o = origin + toPos(i) * childSize;
-      if (pGrid.queryVoxel(o)) children[i] = sSolid;
-      else children[i] = sAir;
-    }
+  if (pGrid.getVoxelCount() == 0 || (pGrid.getVoxelCount() == pGrid.getVolume() && pGrid.isRegionAllSame(glm::uvec3(0), mResolution)) ) {
+    mNodes.emplace_back(mPalette.at(pGrid.queryVoxelData(glm::uvec3(0))));
+    *pCompletedCount += (std::log2(mResolution) + 1) * pGrid.getVolume();
     return;
   }
 
-  for (uint i = 0; i < 8; ++i) {
-    uint64_t total = pCompletedCount + childVolume;
+  *pCompletedCount += mResolution * mResolution * mResolution;
 
-    glm::uvec3 o = origin + toPos(i) * childSize;
+  std::vector<std::tuple<uint, glm::uvec3, uint>> queue;
 
-    bool allZero = true, allOne = true;
+  mNodes.emplace_back(std::make_shared<Node>());
+  for (uint8_t i = 0; i < 8; ++i) queue.push_back({i, toChildPos(i) * (mResolution >> 1), mResolution >> 1});
 
-    if (childSize == 2)
-      for (uint z = o.z; z < o.z + childSize && (allOne || allZero); z += 2)
-        for (uint y = o.y; y < o.y + childSize && (allOne || allZero); y += 2)
-          for (uint x = o.x; x < o.x + childSize && (allOne || allZero); x += 2,pCompletedCount += 8) {
-            const uint8_t* v = reinterpret_cast<uint8_t*>(pGrid.getVoxelDataByte({x, y, z}));
-            allZero = allZero && *v == 0;
-            allOne = allOne && *v == std::numeric_limits<uint8_t>::max();
-          }
-    else
-      for (uint z = o.z; z < o.z + childSize && (allOne || allZero); z += 4)
-        for (uint y = o.y; y < o.y + childSize && (allOne || allZero); y += 4)
-          for (uint x = o.x; x < o.x + childSize && (allOne || allZero); x += 4,pCompletedCount += 64) {
-            const uint64_t* v = reinterpret_cast<uint64_t*>(pGrid.getVoxelDataByte({x, y, z}));
-            allZero = allZero && *v == 0;
-            allOne = allOne && *v == std::numeric_limits<uint64_t>::max();
-          }
+  for (uint i = 0; i < queue.size(); ++i) {
+    uint               index = std::get<0>(queue.at(i));
+    const glm::uvec3  origin = std::get<1>(queue.at(i));
+    uint                size = std::get<2>(queue.at(i));
 
-    pCompletedCount = total;
-
-    if (allZero) children[i] = sAir;
-    else if (allOne) children[i] = sSolid;
-
-    if (allZero || allOne) {
-      pCompletedCount += std::log2(childSize) * childVolume;
+    if (pGrid.isRegionAllSame(origin, size)) {
+      mNodes.at(index >> 3)->children[index & 0b111] = mPalette.at(pGrid.queryVoxelData(origin));
+      *pCompletedCount += (std::log2(size) + 1) * size * size * size;
       continue;
     }
 
-    children[i] = new Node(false, false, childSize, o);
-    pQueue.push_back(children[i]);
+    size >>= 1;
+    for (uint j = 0; j < 8; ++j)
+      queue.push_back(std::make_tuple(mNodes.size() << 3 | j, origin + toChildPos(j) * size, size));
+    std::shared_ptr<Node> n = std::make_shared<Node>();
+    mNodes.emplace_back(n);
+    mNodes.at(index >> 3)->children[index & 0b111] = n;
+    *pCompletedCount += size * size * size << 3;
+  }
+
+// Node* generateImpl(const glm::uvec3& pOrigin, uint pSize) {
+//   if (pGrid.isRegionAllSame(pOrigin, pSize))
+//     return &mPalette.at(pGrid.queryVoxelData(pOrigin));
+
+//   pSize >>= 1;
+//   Node* n = new Node;
+//   mNodes.emplace_back(n);
+//   for (uint i = 0; i < 8; ++i)
+//     n->children[i] = generateImpl(pOrigin + toChildPos(i) * pSize, pSize);
+//   return n;
+// }
+}
+
+void Octree::attach(Octree& pOctree, glm::uvec3& pOrigin) {
+  if (pOctree.getResolution() > mResolution) throw std::runtime_error("Can't attach a larger octree");
+  glm::uvec3 o(0);
+  uint size = mResolution;
+  std::shared_ptr<Node>& n = mNodes.size() == 0 ? mNodes.emplace_back(std::make_shared<Node>()) : mNodes.at(0);
+  while ((size >>= 1) != (pOctree.getResolution() >> 1)) {
+    uint i = toChildIndex((pOrigin - o) / size);
+    o = toChildPos(i) * size;
+    if (!n->children[i]) n->children[i] = std::make_shared<Node>();
+    n = n->children[i];
+  }
+  n = pOctree.mNodes.at(0);
+}
+
+std::vector<std::array<uint32_t, 8>> Octree::generateIndices() {
+  std::vector<std::array<uint32_t, 8>> indices;
+  std::vector<std::array<uint32_t*, 8>> arangement;
+  std::vector<std::shared_ptr<Node>> queue;
+
+  processNode(mNodes.at(0), arangement, queue); // Process root node
+  
+  for (uint i = 0; i < queue.size(); ++i)
+    processNode(queue[i], arangement, queue);
+
+  indices.resize(arangement.size());
+  for (uint i = 0; i < arangement.size(); ++i)
+    for (uint j = 0; j < 8; ++j)
+      indices[i][j] = *arangement[i][j];
+
+  return indices;
+}
+
+void Octree::resizePalette(uint pSize) {
+  mPalette.erase(mPalette.begin() + pSize, mPalette.end());
+  for (uint i = 0; i < mPalette.size(); ++i)
+    mPalette[i]->index = std::numeric_limits<uint32_t>::max() - mPalette.size() + i;
+}
+
+uint Octree::getResolution() {
+  return mResolution;
+}
+
+void Octree::write(std::string pPath) {
+  // Write octree
+  pPath.append(".vm8");
+  std::println("Generating indices");
+  VMesh::Timer t;
+  std::vector<std::array<uint32_t, 8>> indices = generateIndices();
+  std::println("Generating indices took: {}", t.getTime());
+
+  t.start();
+  std::println("Writing octree to: {}", pPath);
+
+  std::ofstream fout;
+  fout.open(pPath, std::ios::out | std::ios::binary);
+  if (!fout.is_open()) throw std::runtime_error("Could not open output file");
+
+  // Header
+  fout << "VMeshOctree\n0100\n";
+  // Resolution
+  fout.write(reinterpret_cast<char*>(&mResolution), sizeof(uint32_t));
+  // Palette size
+  uint32_t paletteSize = mPalette.size();
+  fout.write(reinterpret_cast<char*>(&paletteSize), sizeof(uint32_t));
+  // Indices
+  uint32_t indicesSize = indices.size();
+  fout.write(reinterpret_cast<char*>(&indicesSize), sizeof(uint32_t));
+  fout.write(reinterpret_cast<char*>(indices.data()), indices.size() * 8ull * sizeof(uint32_t));
+
+  fout.close();
+  std::println("Writing took: {}", t.getTime());
+}
+
+void Octree::processNode(std::shared_ptr<Node> pNode, std::vector<std::array<uint32_t*, 8>>& pArangement, std::vector<std::shared_ptr<Node>>& pQueue) {
+  pNode->index = pArangement.size();
+  pArangement.emplace_back();
+  for (uint i = 0; i < 8; ++i) {
+    std::shared_ptr<Node> n = pNode->children[i] ? pNode->children[i] : mPalette.at(0);
+    pArangement.back()[i] = &n->index;
+    if (n->index < std::numeric_limits<uint32_t>::max() - mPalette.size()) // Isnt leaf
+      pQueue.push_back(n);
   }
 }
 
-void Node::generateBottomUp(VMesh::VoxelGrid& pGrid, std::vector<Node*>& pQueue, uint64_t& pCompletedCount) {
-}
-
-uint Node::toChildIndex(glm::uvec3 pPos) {
+uint Octree::toChildIndex(const glm::uvec3& pPos) {
   glm::tvec3<int, glm::packed_highp> localChildPos = {
     int(std::min(1.0, floor(pPos.x))),
     int(std::min(1.0, floor(pPos.y))),
@@ -90,105 +156,10 @@ uint Node::toChildIndex(glm::uvec3 pPos) {
   return (localChildPos.x << 0) | (localChildPos.y << 1) | (localChildPos.z << 2); // Index in childIndices 0 - 7
 }
 
-glm::uvec3 Node::toPos(uint8_t pChildIndex) {
+glm::uvec3 Octree::toChildPos(uint8_t pChildIndex) {
   glm::uvec3 pos;
   pos.x = 1 & pChildIndex;
   pos.y = ((1 << 1) & pChildIndex) != 0;
   pos.z = ((1 << 2) & pChildIndex) != 0;
   return pos;
 }
-
-void Node::destroy() {
-  delete sAir;
-  delete sSolid;
-}
-
-SparseVoxelOctree::SparseVoxelOctree(uint pSize)
-:mSize(pSize), mMaxDepth(std::log2f(pSize)) {
-  mRootNode = new Node(false, false, mSize, glm::uvec3(0));
-}
-
-SparseVoxelOctree::SparseVoxelOctree(VMesh::VoxelGrid& pGrid, uint64_t* pCompletedCount)
-:mSize(pGrid.getResolution()), mMaxDepth(pGrid.getMaxDepth()) {
-  mRootNode = new Node(false, false, mSize, glm::uvec3(0));
-
-  uint64_t completedCount = 0;
-  if (!pCompletedCount) pCompletedCount = &completedCount;
-
-  std::vector<Node*> queue;
-  mRootNode->generate(pGrid, queue, *pCompletedCount);
-  for (uint64_t i = 0; i < queue.size(); ++i)
-    queue[i]->generate(pGrid, queue, *pCompletedCount);
-}
-
-void SparseVoxelOctree::attachSVO(SparseVoxelOctree& pSVO, const glm::uvec3& pOrigin) {
-  if (pSVO.mSize == mSize) {
-    mRootNode = pSVO.mRootNode;
-    return;
-  }
-  bool allAir = true;
-  for (Node* n: pSVO.mRootNode->children)
-    allAir = allAir && n->isLeaf && n->isAir;
-  if (allAir) return;
-
-  Node** node = &mRootNode;
-  for (uint nodeSize = mSize >> 1; nodeSize >= pSVO.mSize; nodeSize >>= 1) {
-    glm::uvec3 o = (pOrigin - (*node)->origin) / nodeSize;
-    glm::uvec3 parentNodeO = (*node)->origin;
-    node = &(*node)->children[Node::toChildIndex(o)];
-    if ((*node)->isLeaf || !*node)
-      *node = new Node(true, false, nodeSize, parentNodeO + o * nodeSize);
-    (*node)->isLeaf = false;
-  }
-  *node = pSVO.mRootNode;
-}
-
-std::vector<std::array<uint32_t, 8>> SparseVoxelOctree::generateIndices() {
-  // Create nodes
-  std::vector<std::array<uint32_t, 8>> indices;
-  std::vector<Node*> queue;
-  indices.emplace_back();
-  mRootNode->index = 0;
-  mRootNode->createChildren(indices, queue);
-  for (uint64_t i = 0; i < queue.size(); ++i)
-    queue[i]->createChildren(indices, queue);
-  // Evaluate indices
-  mRootNode->evaluateChildrenIndices(indices);
-  for (uint64_t i = 0; i < queue.size(); ++i)
-    queue[i]->evaluateChildrenIndices(indices);
-  return indices;
-}
-
-void SparseVoxelOctree::write(const std::string& pPath) {
-  VMesh::Timer t;
-  std::println("Generating indices");
-  std::vector<std::array<uint32_t, 8>> indices = generateIndices();
-  std::println("Generating indices took: {}", t.getTime());
-
-  t.start();
-  std::println("Writing");
-
-  std::ofstream fout;
-  fout.open(pPath, std::ios::out | std::ios::binary);
-  if (!fout.is_open()) throw "Could not open output file";
-
-  fout.write(reinterpret_cast<char*>(&mSize), sizeof(uint32_t));
-  uint32_t paletteSize = 1;
-  fout.write(reinterpret_cast<char*>(&paletteSize), sizeof(uint32_t));
-  uint32_t indicesSize = indices.size();
-  fout.write(reinterpret_cast<char*>(&indicesSize), sizeof(uint32_t));
-  fout.write(reinterpret_cast<char*>(&indices.at(0)), indices.size() * 8ull * sizeof(uint32_t));
-
-  fout.close();
-  std::println("Writing took: {}", t.getTime());
-}
-
-void SparseVoxelOctree::destroy() {
-  std::vector<Node*> queue;
-  queue.push_back(mRootNode);
-  for (uint64_t i = 0; i < queue.size(); ++i) {
-    for (Node* n: queue.at(i)->children) if (!n->isLeaf) queue.push_back(n);
-    delete queue.at(i);
-  }
-}
-
